2272412345d143acb0771867d79272ff4a9c84e4
Saúl Palacios <palacios22c@users.noreply.github.com>
2024-11-30 22:36:52 +0100

    Update create_USPresidents.sql

--- create_USPresidents.sql
+++ create_USPresidents.sql
@@ -17,7 +17,7 @@ CREATE TABLE IF NOT EXISTS USPresidents (ID INTEGER PRIMARY KEY,
 	assassinated BOOLEAN,			/* 0 = false & 1 = true */
 	religiousAffiliation TEXT);
 
-/* US Presidents (until 2010) */
+/* US Presidents (until 2024) */
 
 INSERT INTO USPresidents VALUES (1, "Washington", "George", "", 1, 2/22/1732, 12/14/1799, "Westmoreland County", "Virginia", "Virginia", "Independent", 4/30/1789, 3/4/1797, 0, 0, "Deist/Episcopalian");
 INSERT INTO USPresidents VALUES (2, "Adams", "John", "", 2, 10/30/1735, 7/4/1826, "Quincy", "Massachusetts", "Massachusetts", "Federalist", 3/4/1797, 3/4/1801, 0, 0, "Unitarian");
@@ -62,6 +62,6 @@ INSERT INTO USPresidents VALUES (40, "Reagan", "Ronald", "Wilson", 40, 02/06/191
 INSERT INTO USPresidents VALUES (41, "Bush", "George", "Herbert Walker", 41, 06/12/1924, , "Milton", "Massachusetts", "Texas", "Republican", 1/20/1989, 1/20/1993, 0, 0, "Episcopalian");
 INSERT INTO USPresidents VALUES (42, "Clinton", "William", "Jefferson", 42, 8/19/1946, , "Hope", "Arkansas", "Arkansas", "Democratic", 1/20/1993, 1/20/2001, 0, 0, "Baptist");
 INSERT INTO USPresidents VALUES (43, "Bush", "George", "Walker", 43, 07/06/1946, , "New Haven", "Connecticut", "Texas", "Republican", 1/20/2001, 1/20/2009, 0, 0, "Methodist");
-INSERT INTO USPresidents VALUES (44, "Obama", "Barack", "Hussein", 44, 08/04/1961, , "Honolulu", "Hawaii", "Illinois", "Democratic", 1/20/2009, 1/20/2017, 1, 0, "Unaffiliated Christian");
-INSERT INTO USPresidents VALUES (45, "Trump", "Donald", "John", 45, 06/14/1946, , "New York", "New York", "New York", "Republican", 1/20/2017, 1/20/2021, 1, 0, "Unaffiliated Christian");
-INSERT INTO USPresidents VALUES (46, "Biden", "Joseph", "Robinette", 46, 11/20/1942, , "Scranton", "Pennsylvania", "Delaware", "Democratic", 1/20/2021, , , , "Unaffiliated Christian");
+INSERT INTO USPresidents VALUES (44, "Obama", "Barack", "Hussein", 44, 08/04/1961, , "Honolulu", "Hawaii", "Illinois", "Democratic", 1/20/2009, 1/20/2017, 0, 0, "United Church of Christ");
+INSERT INTO USPresidents VALUES (45, "Trump", "Donald", "John", 45, 06/14/1946, , "New York", "New York", "New York", "Republican", 1/20/2017, 1/20/2021, 1, 0, "Christian");
+INSERT INTO USPresidents VALUES (46, "Biden", "Joseph", "Robinette", 46, 11/20/1942, , "Scranton", "Pennsylvania", "Delaware", "Democratic", 1/20/2021, , , , "Catholic");
